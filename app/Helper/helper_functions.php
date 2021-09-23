abb420e0f2b0affdd85287a75d6d7267c2817930
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-09-23 17:00:16 +0200

    misc

---
+++ app/Helper/helper_functions.php
@@ -0,0 +1,14 @@
+<?php
+if (!function_exists('voting_result_bar')) {
+    function voting_result_bar(float $yesVotes, float $noVotes): string
+    {
+        $progress = round($yesVotes / ($yesVotes + $noVotes), 2)*100;
+
+        return sprintf(
+            '[%s%s] %s',
+            str_repeat('Y', $progress / 4),
+            str_repeat('N', (100 - $progress) / 4),
+            $progress . '% agree'
+        );
+    }
+}
