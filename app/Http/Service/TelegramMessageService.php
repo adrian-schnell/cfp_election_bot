6992438e306dbca96b0a4fdb2174d223d3cbcec8
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-09-23 17:10:27 +0200

    add Telegram Message Service

---
+++ app/Http/Service/TelegramMessageService.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Service;
+
+use App\Models\TelegramUser;
+use BotMan\BotMan\BotMan;
+use BotMan\BotMan\Exceptions\Base\BotManException;
+use BotMan\Drivers\Telegram\TelegramDriver;
+use Log;
+use Str;
+
+class TelegramMessageService
+{
+    protected BotMan $botman;
+
+    public function __construct(BotMan $bot)
+    {
+        $this->botman = $bot;
+    }
+
+    public function sendMessage(TelegramUser $user, string $message, array $param = ['parse_mode' => 'Markdown']): bool
+    {
+        try {
+            $this->botman->say(
+                $this->escapeMessage($message),
+                $user->telegramId,
+                TelegramDriver::class,
+                $param
+            );
+
+            return true;
+        } catch (BotManException $e) {
+            Log::error('sending botman message failed', [
+                'message'          => $e->getMessage(),
+                'line'             => $e->getLine(),
+                'telegram_user_id' => $user->id,
+                'message_to_user'  => $message,
+            ]);
+
+            return false;
+        }
+    }
+
+    protected function escapeMessage(string $message): string
+    {
+        return Str::replace('_', '\\_', $message);
+    }
+}
