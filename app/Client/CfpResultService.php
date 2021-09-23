243ac87bf13460d9fe6094bdbb470680c50d6c41
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-09-23 16:59:42 +0200

    add cfp result client

---
+++ app/Client/CfpResultService.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Client;
+
+use GuzzleHttp\Client;
+
+class CfpResultService
+{
+    protected Client $client;
+
+    public function __construct()
+    {
+        $this->client = new Client([
+            'base_uri' => 'https://api.dfx.swiss/v1/statistic/cfp/',
+        ]);
+    }
+
+    /**
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function getAllCurrentCfp(): array
+    {
+        $response = $this->client->get('all');
+
+        return json_decode($response->getBody()->getContents(), true);
+    }
+}
