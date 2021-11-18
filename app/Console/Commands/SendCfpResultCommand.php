2a4d980f0b16d2b72c36e72e70cde9fbf15d9398
Adrian <322735+adrian-schnell@users.noreply.github.com>
2021-11-18 16:32:44 +0100

    optimize command

--- app/Console/Commands/SendCfpResultCommand.php
+++ app/Console/Commands/SendCfpResultCommand.php
@@ -12,86 +12,91 @@ use Illuminate\Console\Command;
 
 class SendCfpResultCommand extends Command
 {
-    protected $signature = 'cfp_result:send_updates';
-    protected $description = 'Send the current results to the users';
+	protected $signature = 'cfp_result:send_updates';
+	protected $description = 'Send the current results to the users';
 
-    public function handle(
-        TelegramUserRepository $repository,
-        TelegramMessageService $messageService,
-        CfpMessageRepository $messageRepository
-    ): void {
-        if ($this->checkVotingStarted($messageService)
-            || $this->checkVotingEnded($messageService)) {
-            return;
-        }
+	public function handle(
+		TelegramUserRepository $repository,
+		TelegramMessageService $messageService,
+		CfpMessageRepository   $messageRepository
+	): void {
+		if (!$this->checkVotingStarted($messageService)
+			|| $this->checkVotingEnded($messageService)) {
+			return;
+		}
 
-        $cfpResults = CfpResult::orderBy('github_issue_id')->get();
-        $recipients = $repository->getUsersForCurrentTime();
-        if (count($recipients) === 0) {
-            $this->info('no recipients selected');
+		$cfpResults = CfpResult::orderBy('github_issue_id')->get();
+		$recipients = $repository->getUsersForCurrentTime();
+		if (count($recipients) === 0) {
+			$this->info('no recipients selected');
 
-            return;
-        }
+			return;
+		}
 
-        // send cfp results for each user, depending on it's CFP selection
-        foreach ($recipients as $recipient) {
-            /** @var TelegramUser $recipient */
-            $message = $messageRepository->getMessageFromCollection($cfpResults, $recipient->cfp_selection);
-            $messageService->sendMessage(
-                [$recipient->telegramId],
-                $message,
-                ['disable_web_page_preview' => true, 'parse_mode' => 'Markdown']
-            );
-        }
-    }
+		// send cfp results for each user, depending on it's CFP selection
+		foreach ($recipients as $recipient) {
+			/** @var TelegramUser $recipient */
+			$message = $messageRepository->getMessageFromCollection($cfpResults, $recipient->cfp_selection);
+			$messageService->sendMessage(
+				[$recipient->telegramId],
+				$message,
+				['disable_web_page_preview' => true, 'parse_mode' => 'Markdown']
+			);
+		}
+	}
 
-    protected function checkVotingStarted(TelegramMessageService $messageService): bool
-    {
-        // inform users onetime after voting started
-        $cacheKeyVotingStarted = sprintf('voting_%s_started', config('cfp_settings.cfp_round'));
-        $startDate = config('cfp_settings.start_date');
-        if (now() >= $startDate
-            && now()->diffInMinutes(Carbon::parse($startDate)) < 30
-            && !cache($cacheKeyVotingStarted, false)) {
-            $this->info('voting started info to all users');
-            $votingStartedRecipients = TelegramUser::all()->pluck('telegramId')->toArray();
-            $messageService->sendMessage(
-                $votingStartedRecipients,
-                sprintf("*Yehaa* - the new CFP Votings *%s* just started!", config('cfp_settings.cfp_round')),
-                ['parse_mode' => 'Markdown']
-            );
-            cache([$cacheKeyVotingStarted => true]);
+	protected function checkVotingStarted(TelegramMessageService $messageService): bool
+	{
+		// inform users onetime after voting started
+		$cacheKeyVotingStarted = sprintf('voting_%s_started', config('cfp_settings.cfp_round'));
+		$startDate = config('cfp_settings.start_date');
+		if (now() >= $startDate
+			&& now()->diffInMinutes(Carbon::parse($startDate)) < 30) {
 
-            return true;
-        }
+			if (!cache($cacheKeyVotingStarted, false)) {
+				$this->info('voting started info to all users');
+				$votingStartedRecipients = TelegramUser::all()->pluck('telegramId')->toArray();
+				$messageService->sendMessage(
+					$votingStartedRecipients,
+					sprintf("*Yehaa* - the new CFP Votings *%s* just started!\r\n\r\nSelect the CFP/DFIP you want to monitor with the command /settings",
+						config
+						('cfp_settings.cfp_round')),
+					['parse_mode' => 'Markdown']
+				);
+				cache([$cacheKeyVotingStarted => true]);
+			}
 
-        return false;
-    }
+			return true;
+		}
 
-    protected function checkVotingEnded(TelegramMessageService $messageService): bool
-    {
-        // stop this command after the voting ended, inform users onetime
-        if (now() > config('cfp_settings.end_date')) {
-            $cacheKeyVotingEnded = sprintf('voting_%s_ended', config('cfp_settings.cfp_round'));
-            if (!cache($cacheKeyVotingEnded, false)) {
-                $votingStartedRecipients = TelegramUser::all()->pluck('telegramId')->toArray();
+		return false;
+	}
 
-                $message = sprintf("*It's done...* \r\n\r\n the new CFP Votings *%s* just ended!",
-                    config('cfp_settings.cfp_round'));
-                $message .= "\r\n\r\n To see the result use the command /cfp_all";
-                $messageService->sendMessage(
-                    $votingStartedRecipients,
-                    $message,
-                    ['parse_mode' => 'Markdown']
-                );
-                cache([$cacheKeyVotingEnded => true]);
-                return false;
-            }
-            $this->info('voting ended info to all users');
+	protected function checkVotingEnded(TelegramMessageService $messageService): bool
+	{
+		// stop this command after the voting ended, inform users onetime
+		if (now() > config('cfp_settings.end_date')) {
+			$cacheKeyVotingEnded = sprintf('voting_%s_ended', config('cfp_settings.cfp_round'));
+			if (!cache($cacheKeyVotingEnded, false)) {
+				$votingStartedRecipients = TelegramUser::all()->pluck('telegramId')->toArray();
 
-            return true;
-        }
+				$message = sprintf("*It's done...* \r\n\r\n the new CFP Votings *%s* just ended!",
+					config('cfp_settings.cfp_round'));
+				$message .= "\r\n\r\n To see the result use the command /cfp_all";
+				$messageService->sendMessage(
+					$votingStartedRecipients,
+					$message,
+					['parse_mode' => 'Markdown']
+				);
+				cache([$cacheKeyVotingEnded => true]);
 
-        return false;
-    }
+				return false;
+			}
+			$this->info('voting ended info to all users');
+
+			return true;
+		}
+
+		return false;
+	}
 }
