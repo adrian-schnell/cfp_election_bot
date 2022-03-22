f1db303ccfde9f31b4b3107f588a7d0ced580ac1
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-03-22 09:28:04 +0100

    update for current voting round

--- config/cfp_settings.php
+++ config/cfp_settings.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'start_date' => '2022-02-07 23:59:59',
-    'end_date'   => '2022-02-14 23:59:59',
-    'cfp_round'  => 'February 2022',
+    'start_date' => '2022-03-21 23:59:59',
+    'end_date'   => '2022-03-28 23:59:59',
+    'cfp_round'  => 'March 2022',
 ];
