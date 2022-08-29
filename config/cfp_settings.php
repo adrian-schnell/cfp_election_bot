3eda0a9a92332b56bdfb8132683cace568fa7eb0
Adrian <322735+adrian-schnell@users.noreply.github.com>
2022-08-29 22:12:52 +0200

    update for august round

--- config/cfp_settings.php
+++ config/cfp_settings.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'start_date' => '2022-07-04 23:59:59',
-    'end_date'   => '2022-07-11 23:59:59',
-    'cfp_round'  => 'July 2022',
+    'start_date' => '2022-08-29 23:59:59',
+    'end_date'   => '2022-09-05 23:59:59',
+    'cfp_round'  => 'August 2022',
 ];
